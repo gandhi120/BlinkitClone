88c8c55685ba38ac9711c7154ba8157238f1a6d2
varun-gandhi-digiqc <varun@digiqc.com>
2024-10-20 23:49:36 +0530

    -add custom safe area view.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import 'react-native-gesture-handler';
 import Navigation from '@navigation/Navigation';
 import { Provider } from 'react-redux';
 import {store,persistor} from '@store/store';
 import { PersistGate } from 'redux-persist/integration/react';
+
 
 const App = ()=>{
   return(
--- src/views/auth/CustomerLogin.tsx
+++ src/views/auth/CustomerLogin.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { FC } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import CustomSafeAreaView from '@views/component/CustomSafeAreaView';
 
-const CustomerLogin = ()=> {
+const CustomerLogin:FC = ()=> {
     return (
-      <View>
+      <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+      <CustomSafeAreaView>
         <Text> CustomerLogin </Text>
+        </CustomSafeAreaView>
       </View>
+      </GestureHandlerRootView>
     );
 };
+
+const styles = StyleSheet.create({
+  container:{
+      flex:1,
+      backgroundColor:'#faf',
+  },
+});
 export default CustomerLogin;
