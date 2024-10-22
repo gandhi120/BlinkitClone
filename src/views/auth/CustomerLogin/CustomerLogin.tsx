9988ff56edec8cf447e613058b414b7522cb0d0d
varun-gandhi-digiqc <varun@digiqc.com>
2024-10-23 00:26:11 +0530

    -set animation logic.

--- src/views/auth/CustomerLogin/CustomerLogin.tsx
+++ src/views/auth/CustomerLogin/CustomerLogin.tsx
@@ -1,16 +1,50 @@
-import React, { FC } from 'react';
-import { View } from 'react-native';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import React, { FC, useState } from 'react';
+import { Animated, Image, View } from 'react-native';
+import {GestureHandlerRootView, PanGestureHandler, State} from 'react-native-gesture-handler';
 import CustomSafeAreaView from '@views/component/CustomSafeAreaView';
 import ProductSlider from '@views/component/login/ProductSlider/productSlider';
 import styles from './styles';
+import { resetAndNavigate } from '@utils/NavigationUtils';
 
 const CustomerLogin:FC = ()=> {
+
+  const[gestureSequence,setGestureSequence] = useState<string[]>([]);
+
+  const handleGesture = ({nativeEvent}:any)=>{
+    if(nativeEvent.state === State.END){
+      const{translationX,translationY} = nativeEvent;
+      let direction = '';
+      if(Math.abs(translationX) > Math.abs(translationY)){
+        direction = translationX > 0 ? 'right' : 'left';
+      }else{
+        direction = translationY > 0 ? 'down' : 'up';
+    }
+    const newSequence = [...gestureSequence,direction].slice(-5);
+    setGestureSequence(newSequence);
+    if(newSequence.join(' ') === 'up up down left right'){
+      setGestureSequence([]);
+      resetAndNavigate('DeliveryLogin');
+    }
+    }
+  };
+
     return (
       <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
       <CustomSafeAreaView>
         <ProductSlider/>
+        <PanGestureHandler onHandlerStateChange={handleGesture}>
+          <Animated.ScrollView
+          bounces={false}
+          keyboardDismissMode={'on-drag'}
+          keyboardShouldPersistTaps={'handled'}
+          contentContainerStyle={styles.subContainer}
+          >
+            <View style={styles.content}>
+              <Image source={require('@assets/images/logo.png')} style={styles.logo}/>
+            </View>
+          </Animated.ScrollView>
+        </PanGestureHandler>
         </CustomSafeAreaView>
       </View>
       </GestureHandlerRootView>
--- src/views/auth/CustomerLogin/styles.tsx
+++ src/views/auth/CustomerLogin/styles.tsx
@@ -1,9 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container:{
         flex:1,
         backgroundColor:'white',
+    },
+    subContainer:{
+        flex:1,
+        justifyContent:'flex-end',
+        alignItems:'center',
+        marginBottom:20,
+    },
+    content:{
+        justifyContent:'center',
+        alignItems:'center',
+        width:'100%',
+        backgroundColor:'white',
+        paddingHorizontal:20,
+        paddingBottom:20,
+    },
+    logo:{
+        height:50,
+        width:50,
+        borderRadius:20,
+        marginVertical:10,
     },
   });
 
